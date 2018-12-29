ed38dd627e0c7de754adc9f37fa445e21dacd082
Bryce Eadie <bryce.eadie@gmail.com>
2018-12-28 20:09:02 -0800

    Add name validations to Runner model

--- app/models/runner.rb
+++ app/models/runner.rb
@@ -1,7 +1,18 @@
 class Runner < ActiveRecord::Base
   has_secure_password
-  
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
   has_many :runs
   has_many :goals, through: :runs
   has_many :followups, through: :runs
 end
+
+
+# validates :title, presence: true
+# validates :title, uniqueness: {
+#   scope: :release_year, message: "cannot have same title in same year"
+# }
+# validates :artist_name, presence: true
+# validates :released, inclusion: { in: [true, false] }
+# validate :release_year_legit
