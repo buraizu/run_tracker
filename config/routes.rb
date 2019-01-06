95e3a85f1872350a55b06bff6bf4a752047f685c
Bryce Eadie <bryce.eadie@gmail.com>
2019-01-06 02:30:52 -0800

    Separate Login and Signup pages, add styles to New Runner and login pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'runners#new'
+  root 'sessions#new'
 
   resources :runners do
     resources :runs
