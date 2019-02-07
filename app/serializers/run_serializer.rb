f0993212bc5bbc294ae0c0d2c6ac0ccb5561c0e3
Bryce Eadie <bryce.eadie@gmail.com>
2019-02-06 17:47:46 -0800

    Add attributes to Run serializer

--- app/serializers/run_serializer.rb
+++ app/serializers/run_serializer.rb
@@ -1,3 +1,12 @@
 class RunSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :course, :distance, :time, :review, :rating
+  belongs_to :runner
 end
+
+t.integer "runner_id"
+t.string "course"
+t.float "distance"
+t.integer "time"
+t.text "review"
+t.integer "rating"
+t.index ["runner_id"], name: "index_runs_on_runner_id"
